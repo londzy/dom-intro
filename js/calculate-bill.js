758f84b5ae9711ce0571832f80cf6d69872c7cf8
Londiwe <celelondiwe3@gmail.com>
2018-04-16 15:15:33 +0200

    did totalBill

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,10 +1,51 @@
 //get a reference to the calculate button
-
+var calculateBtn = document.querySelector('.calculateBtn');
 //get a reference to the billTotal element
-
+var billTotalElement = document.querySelector('.billTotal');
 //get a reference to the billString
-
+var billStringField = document.querySelector('.billString');
 //create the function that will be called when the calculate button is pressed
+
+calculateBtn.addEventListener('click', calculateBtnClicked) ;
+function calculateBtnClicked(){
+    // get the string entered in the textArea
+    var billString = billStringField.value;
+    //split the string
+    var billItems = billString.split(",");
+    // a variable for the total phone bill.
+    var billTotal = 0;
+    //loop over all the bill items
+    for (var i=0;i<billItems.length;i++){
+        var billItem = billItems[i].trim();
+        if (billItem === "call"){
+            billTotal += 2.75;
+        }
+        else if (billItem === "sms"){
+            billTotal += 0.75;
+        }
+    }
+
+    //round to two decimals
+    var roundedBillTotal = billTotal.toFixed(2);
+    billTotalElement.innerHTML = roundedBillTotal;
+
+    if (billTotal >= 30){
+        // adding the danger class will make the text red
+        billTotalElement.classList.add("danger");
+    }
+    else if (billTotal >= 20){
+        billTotalElement.classList.add("warning");
+    }
+    if (billTotal <= 30){
+        // adding the danger class will make the text red
+        billTotalElement.classList.remove("danger");
+      }
+      if (billTotal <= 20){
+          // adding the danger class will make the text red
+          billTotalElement.classList.remove("warning");
+        }
+}
+
 //  * this function should read the string value entered - split it on a comma.
 //  * loop over all the entries in the the resulting list
 //  * check if it is a call or an sms and add the right amount to the overall total
